e2cc182bd5d83607f39d5baacb05afadc38b2ddf
Efnilite <re.verstappen@gmail.com>
2022-05-06 16:43:23 +0200

    Remove unused method

--- src/main/java/dev/efnilite/worldserver/util/Util.java
+++ src/main/java/dev/efnilite/worldserver/util/Util.java
@@ -35,16 +35,4 @@ public class Util {
         }
         return new ArrayList<>(section.getKeys(false));
     }
-
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-
-        Map<K, V> result = new LinkedHashMap<>();
-        for (Map.Entry<K, V> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-
-        return result;
-    }
 }
